fdf31cf5ba802908fb565c32e6f297a62033a69f
joao.meyer <joaoparizotimeyer@gmail.com>
2024-04-06 14:51:40 -0300

    <master> adding database creation sql.

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE `estacionamento` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+-- estacionamento.vagas definition
+
+CREATE TABLE `vagas` (
+  `posicao` varchar(100) NOT NULL,
+  `ocupada` tinyint(1) NOT NULL,
+  `nome` varchar(250) DEFAULT NULL,
+  `ondeesta` varchar(250) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
